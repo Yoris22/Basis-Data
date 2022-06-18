47c68797c9d2db863b3fc40677fe638d99d4f98a
dayyan-269 <101689434+dayyan-269@users.noreply.github.com>
2022-06-18 21:37:00 +0700

    add comment

--- index.php
+++ index.php
@@ -1,49 +1,50 @@
-    <?php
+<?php
     include "config.php";
-    ?>
-    <!DOCTYPE html>
-    <html>
+?>
+<!DOCTYPE html>
+<html>
 
-    </head>
-        <tittle> Basis Data 2 phpmyadmin</tittle>
-    </head>
+</head>
+    <tittle> Basis Data 2 phpmyadmin</tittle>
+</head>
 
-    <body>
-        <!-- INI TABLE PEGAWAI -->
-        <h3>Isi Tabel Pegawai</h3>
-        <table border="1">
-            <thead>
+<body>
+    <!-- INI TABLE PEGAWAI -->
+    <h3>Isi Tabel Pegawai</h3>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>NIP</th>
+                <th>NAMA</th>
+                <th>KOTA</th>
+                <th>TGL_LAHIR</th>
+                <th>JK</th>
+                <th>KJ</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $sql = "SELECT * FROM pegawai";
+                $query = mysqli_query($db, $sql);
+            ?>
+            <?php while ($hasil = mysqli_fetch_array($query)): ?>
                 <tr>
-                    <th>NIP</th>
-                    <th>NAMA</th>
-                    <th>KOTA</th>
-                    <th>TGL_LAHIR</th>
-                    <th>JK</th>
-                    <th>KJ</th>
+                    <!-- <?= '' ?> itu kependekan dari <?php echo '' ?> -->
+                    <td><?= $hasil['nip'] ?></td> 
+                    <td><?= $hasil['nama'] ?></td>
+                    <td><?= $hasil['kota'] ?></td>
+                    <td><?= $hasil['tgl_lahir'] ?></td>
+                    <?php if ($hasil['jk'] == 'L'): ?>
+                        <td>laki-laki</td>
+                    <?php else: ?>
+                        <td>perempuan</td>
+                    <?php endif; ?>
+                    <td><?= $hasil['kj'] ?></td>";
                 </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $sql = "SELECT * FROM pegawai";
-                    $query = mysqli_query($db, $sql);
-                ?>
-                <?php while ($hasil = mysqli_fetch_array($query)): ?>
-                    <tr>
-                        <td><?= $hasil['nip'] ?></td>
-                        <td><?= $hasil['nama'] ?></td>
-                        <td><?= $hasil['kota'] ?></td>
-                        <td><?= $hasil['tgl_lahir'] ?></td>
-                        <?php if ($hasil['jk'] == 'L'): ?>
-                            <td>laki-laki</td>
-                        <?php else: ?>
-                            <td>perempuan</td>
-                        <?php endif; ?>
-                        <td><?= $hasil['kj'] ?></td>";
-                    </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
-        <p>Total data: <?php echo mysqli_num_rows($query) ?></p>
-        <!-- END TABLE PEGAWAI -->
-    </body>
-    </html>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+    <p>Total data: <?php echo mysqli_num_rows($query) ?></p>
+    <!-- AKHIR TABLE PEGAWAI -->
+</body>
+</html>
